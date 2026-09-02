03cdd791aa8b312092ca7b5f2d9c90270114d57e
Denis Narushevich <d.narushevich@gmail.com>
2026-09-03 00:23:13 +0300

    cleanup

--- src/config.js
+++ src/config.js
@@ -3,5 +3,4 @@ export default {
     layerStrokeMask: parseInt('11',2), //bitmask, bit per layer
     layerClearMask: parseInt('11',2), //bitmask, bit per layer
     layersCount: 1,
-    debug: true
 };
