4f281bf7e6ed20c2e05c3e85741772c857923045
Denis Narushevich <d.narushevich@gmail.com>
2026-09-19 00:50:04 +0300

    fixed example

--- examples/earth/main.js
+++ examples/earth/main.js
@@ -1,5 +1,5 @@
 import scaliaEngine from "sub3d";
-import "sub3d/dist/sub3d.css";
+import "sub3d/sub3d.css";
 import earthTexture from "./earth-texture-equirectangular.jpg";
 import moonTexture from "./2k_moon.jpg";
 
